17eba2d67c9b820463f7ec9ee213b206c6cee6f8
kotwiceal <kotvitskyaya@gmail.com>
2024-04-10 15:35:17 +0300

    implement wifi sta mode

--- main/main.c
+++ main/main.c
@@ -1,6 +1,25 @@
-#include <stdio.h>
+#include <esp_system.h>
+#include <nvs_flash.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_http_server.h"
+#include "esp_timer.h"
 
-void app_main(void)
-{
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
 
+/* user defined libraries */
+#include "wifi.h"
+// #include "http.h"
+// #include "uart.h"
+
+static const char *TAG_MAIN = "MAIN";
+
+void app_main(void) {
+    ESP_LOGI(TAG_MAIN, "initializing");
+
+    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+
+    init_wifi();
 }
